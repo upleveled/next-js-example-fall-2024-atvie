d42e4ab99164b21638bdb6f7b1a0b0178c6e6b49
Lukas Prochazka <prochazka.lu@gmail.com>
2024-10-07 18:56:30 +0200

    Add test

---
+++ util/__tests__/math.test.ts
@@ -0,0 +1,12 @@
+import { expect, test } from '@jest/globals';
+import { add } from '../math';
+
+test('adds two numbers', () => {
+  expect(add(1, 2)).toBe(3);
+  expect(add(100, 200)).toBe(300);
+});
+
+test('throws an error when arguments are not numbers', () => {
+  expect(() => add(1, '1')).toThrow('Pass only numbers!');
+  expect(() => add('asd', '1')).toThrow('Pass only numbers!');
+});
---
+++ util/dataStructures.js
@@ -0,0 +1,23 @@
+export function reduceAnimalsWithFoods(animalsWithFoods) {
+  const animalWithFood = animalsWithFoods[0];
+
+  if (!animalWithFood) {
+    throw new Error('No animal found');
+  }
+
+  const animalWithFoods = {
+    id: animalWithFood.animalId,
+    firstName: animalWithFood.animalFirstName,
+    type: animalWithFood.animalType,
+    accessory: animalWithFood.animalAccessory,
+    birthDate: animalWithFood.animalBirthDate,
+    animalFoods: animalsWithFoods.map((animal) => {
+      return {
+        id: animal.animalFoodId,
+        name: animal.animalFoodName,
+        type: animal.animalFoodType,
+      };
+    }),
+  };
+  return animalWithFoods;
+}
