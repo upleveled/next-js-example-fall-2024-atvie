29ef18e3a269b4ea0958adde75db29d31b85706e
Karl Horky <karl.horky@gmail.com>
2024-11-02 22:53:07 +0100

    Remove export

--- database/users.ts
+++ database/users.ts
@@ -3,7 +3,7 @@ import type { User } from '../migrations/00006-createTableUsers';
 import type { Session } from '../migrations/00007-sessions';
 import { sql } from './connect';
 
-export type UserWithPasswordHash = User & {
+type UserWithPasswordHash = User & {
   passwordHash: string;
 };
 
